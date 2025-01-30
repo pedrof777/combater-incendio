327663f897f0cc20d15c79cec7d93b7076b0fb5d
Pedro <pedrohenriquef278@gmail.com>
2025-01-30 15:14:40 -0300

    production

--- Assets/js/function.js
+++ Assets/js/function.js
@@ -1,30 +1,42 @@
 //Espera o conteudo da pagina carregar
 window.onload = function () {
   //Fecha e abre o menu mobile
+  const menus = document.querySelector(".menu nav ul");
   document.querySelector(".menu_mobile").addEventListener("click", function () {
-    if (document.querySelector(".menu nav ul").style.display == "flex") {
-      document.querySelector(".menu nav ul").style.display = "none";
+    if (menus.style.display == "flex") {
+      menus.style.display = "none";
     } else {
-      document.querySelector(".menu nav ul").style.display = "flex";
+      menus.style.display = "flex";
     }
   });
 
-  //Seleção do menu ativado
-  const menuItems = document.querySelectorAll("nav ul li a");
-  menuItems.forEach((item) => {
-    item.addEventListener("click", function () {
-      // Remove a classe 'active' de todos os itens
-      document.querySelectorAll("nav ul li ").forEach((li) => li.classList.remove("active"));
+  // Seleciona todos os links de navegação
+  const navLinks = document.querySelectorAll("li");
+  navLinks.forEach((link) => {
+    link.addEventListener("click", function () {
+      // Remove a classe 'active' de todos os links
+      navLinks.forEach((link) => link.classList.remove("active"));
+    });
+  });
 
-      // Adiciona a classe 'active' ao item clicado
-      item.parentElement.classList.add("active");
+  // Fecha o menu se a tela for pequena
+  const menuItems = document.querySelectorAll("ul li a");
+  const menu = document.querySelector("ul");
+  const mediaQuery = window.matchMedia("(max-width: 600px)");
+
+  menuItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      if (mediaQuery.matches) {
+        menu.style.display = "none";
+      } else {
+        menu.style.display = "flex";
+      }
     });
   });
 
   //Rolagem suave
   // Seleciona os links de navegação
   const links = document.querySelectorAll("nav ul li a");
-
   links.forEach((link) => {
     // Adiciona o evento de clique em cada link
     link.addEventListener("click", function (event) {
